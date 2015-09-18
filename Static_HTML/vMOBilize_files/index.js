27d7e6a47c0aa005670f64c078511baf1a76140a
James Curtin <jameswcurtin@gmail.com>
2015-09-17 21:57:57 -0400

    Badges page for Gov92r

---
+++ Static_HTML/vMOBilize_files/index.js
@@ -0,0 +1,13 @@
+/*------------------------------------------------------------------
+Project:    Oneline
+Author:     Yevgeny S.
+URL:        https://twitter.com/YevSim
+Version:    1.0
+Created:        28/04/2014
+Last change:    22/07/2014
+-------------------------------------------------------------------*/
+
+/* ===== Tooltips ===== */
+
+$('#tooltip').tooltip();
+
