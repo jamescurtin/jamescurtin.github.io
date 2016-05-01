c86f11b5954d58d0637ea2bc470ad80327def263
James Curtin <jameswcurtin@gmail.com>
2016-04-30 23:03:39 -0400

    Get rid of smooth scroll for now

--- includes/js/smoothscroll.js
+++ includes/js/smoothscroll.js
@@ -1,11 +1,11 @@
-setTimeout(smoothscroll, 750);
-function smoothscroll(){
-	if ($('.custom-page-content')){
-	  $(function(){
-	      $('html, body').animate({
-	          scrollTop: $('.custom-page-content').offset().top-200
-	      }, 2000);
-	      return false;
-	  });
-	}
-}
+// setTimeout(smoothscroll, 750);
+// function smoothscroll(){
+// 	if ($('.custom-page-content')){
+// 	  $(function(){
+// 	      $('html, body').animate({
+// 	          scrollTop: $('.custom-page-content').offset().top-200
+// 	      }, 2000);
+// 	      return false;
+// 	  });
+// 	}
+// }
