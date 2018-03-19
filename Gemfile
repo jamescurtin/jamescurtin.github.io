5230a0759ce815e196fa9bf47a9371acaffe4a41
James Curtin <jameswcurtin@gmail.com>
2018-03-18 22:34:18 -0400

    Add new photos and update jekyll dependencies

--- Gemfile
+++ Gemfile
@@ -1,3 +1,13 @@
-source 'https://rubygems.org'
+source "https://rubygems.org"
 
-gem 'github-pages'
+gem "github-pages", group: :jekyll_plugins
+
+group :jekyll_plugins do
+  gem "jekyll-feed", "~> 0.6"
+  gem "jekyll-redirect-from"
+  gem "jekyll-sitemap"
+  gem "jekyll-seo-tag"
+end
+
+gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem "wdm", "~> 0.1.0" if Gem.win_platform?
--- otherstuff/photo_html_generator.py
+++ otherstuff/photo_html_generator.py
@@ -3,36 +3,36 @@ import csv
 
 # 	List 1 is normal display
 # 	List 2 is the slider
-portrait1 = [1, 149, 103, 118, 2, 3, 146, 4, 119, 10, 20, 22,
-             40, 101, 78, 127, 84, 147, 85, 124, 86, 96,
-             100, 102, 105, 125, 31, 126, 113, 120,
-             122, 123]
-portrait2 = [1, 149, 103, 2, 20, 146, 4, 40, 96, 100, 127, 101,
-             118]
+portrait1 = [
+    1, 149, 103, 118, 2, 3, 146, 4, 119, 10, 20, 22, 40, 101, 78, 127, 84, 147,
+    85, 124, 86, 96, 100, 102, 105, 125, 31, 126, 113, 120, 122, 123,
+]
+portrait2 = [
+    1, 149, 103, 2, 20, 146, 4, 40, 96, 100, 127, 101, 118,
+    ]
 
-people1 = []
-people2 = []
+events1 = [
+    69, 103, 5, 21, 30, 145, 87, 40, 41, 58, 59, 60, 61, 62, 64, 68, 79, 88,
+    89, 116, 92, 93, 95, 99, 100, 101, 104, 105, 110, 115,
+]
+events2 = [69, 62, 61, 40, 60, 145, 100,]
 
-events1 = [69, 103, 5, 21, 30, 145, 87, 40, 41, 58, 59,
-           60, 61, 62, 64, 68, 79, 88, 89, 116, 92,
-           93, 95, 99, 100, 101, 104, 105, 110, 115]
-events2 = [69, 62, 61, 40, 60, 145, 100]
+harvard1 = [
+    6, 7, 8, 9, 111, 121, 11, 58, 68, 79, 90, 27, 91,
+    ]
+harvard2 = [
+    8, 121, 27, 11, 91, 68, 111,
+    ]
 
-harvard1 = [6, 7, 8, 9, 111, 121, 11, 58, 68, 79, 90,
-            27, 91]
-harvard2 = [8, 121, 27, 11, 91, 68, 111]
+nature1 = [
+    152, 12, 142, 129, 139, 15, 130, 16, 132, 18, 19, 134, 136, 25, 28, 144,
+    108, 29, 33, 34, 35, 36, 37, 38, 43, 44, 109, 45, 46, 47, 48, 49, 50, 51,
+    52, 53, 54, 55, 56, 65, 112, 70, 71, 72, 73, 75, 77, 80, 13, 14, 17, 81,
+    82, 117, 83, 42, 57, 150, 151, 153, 154, 155,
+]
+nature2 = [152, 129, 19, 132, 75, 134, 53, 108, 15, 16, 33, 44, 81,]
 
-nature1 = [12, 142, 129, 139, 15, 130, 16, 132, 18, 19, 134, 136, 25, 28, 144, 108, 29, 33,
-           34, 35, 36, 37, 38, 43, 44, 109, 45, 46,
-           47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
-           65, 112, 70, 71, 72, 73, 75, 77, 80, 13,
-           14, 17, 81, 82, 117, 83, 42, 57]
-nature2 = [129, 19, 132, 75, 134, 53, 108, 15, 16, 33, 44, 81]
-
-assorted1 = []
-assorted2 = []
-
-reader = csv.DictReader(open('photoInfo.csv'))
+reader = csv.DictReader(open('../_data/photo_info.csv'))
 result = {}
 for row in reader:
     key = row.pop('id')
@@ -60,8 +60,9 @@ def genHTML(photoList, html):
         for j in shuffled:
             slug = str(j)
             if len(html) == 6:
-                base += (html[0] + slug + html[1] + result[slug]["caption"] + html[2] + slug +
-                         html[3] + result[slug]["alt"] + html[4] + result[slug]["title"] + html[5])
+                base += (html[0] + slug + html[1] + result[slug]["caption"] +
+                         html[2] + slug + html[3] + result[slug]["alt"] +
+                         html[4] + result[slug]["title"] + html[5])
             else:
                 base += html[0] + slug + html[1] + result[slug]["title"] + html[2]
         file = open("../_includes/" + i.name + ".html", "w")
@@ -71,40 +72,39 @@ def genHTML(photoList, html):
 
 portrait = Page("portrait", portrait1)
 portraitslider = Page("portraitslider", portrait2)
-people = Page("people", people1)
-peopleslider = Page("peopleslider", people2)
 events = Page("events", events1)
 eventsslider = Page("eventsslider", events2)
 harvard = Page("harvard", harvard1)
 harvardslider = Page("harvardslider", harvard2)
 nature = Page("nature", nature1)
 natureslider = Page("natureslider", nature2)
-assorted = Page("assorted", assorted1)
-assortedslider = Page("assortedslider", assorted2)
-slider_404 = Page("slider_404", set(people.pictures + events.pictures + harvard.pictures +
-                                    nature.pictures + assorted.pictures))
-mergedlist = Page("mergedlist", list(set(portrait.pictures + people.pictures + events.pictures +
-                                         harvard.pictures + nature.pictures + assorted.pictures)))
+slider_404 = Page("slider_404",
+                  set(events.pictures + harvard.pictures + nature.pictures))
+mergedlist = Page(
+    "mergedlist",
+    list(
+        set(portrait.pictures + events.pictures +
+            harvard.pictures + nature.pictures)))
 
-biglist1 = [portrait, people, events, harvard, nature, assorted, mergedlist]
-biglist2 = [portraitslider, peopleslider, eventsslider, harvardslider, natureslider,
-            assortedslider, slider_404]
+biglist1 = [
+    portrait, events, harvard, nature, mergedlist
+    ]
+biglist2 = [
+    portraitslider, eventsslider, harvardslider, natureslider, slider_404
+    ]
 
-html1 = ['<div class="qg-img" data-defwidth="435"><a href="/includes/img/highres/highres_',
-         '.jpg" data-rel="lightbox[1]" title=" ',
-         '" ><img src="/includes/img/photos/photo_',
-         '.jpg" alt="',
-         '''
+html1 = [
+    '<div class="qg-img" data-defwidth="435"><a href="/includes/img/highres/highres_',
+    '.jpg" data-rel="lightbox[1]" title=" ',
+    '" ><img src="/includes/img/photos/photo_', '.jpg" alt="', '''
             "/><div class="qg-overlay"><span class="icon-circle"><span class="pg-icon lightbox-icon">
             </span></span><span class="qg-title">
-            ''',
-         '</span></div></a></div>'
-         ]
+            ''', '</span></div></a></div>'
+]
 
-html2 = ['<img src="/includes/img/highres/highres_',
-         '.jpg" title="',
-         '" alt="" />'
-         ]
+html2 = [
+    '<img src="/includes/img/highres/highres_', '.jpg" title="', '" alt="" />'
+]
 
 genHTML(biglist1, html1)
 genHTML(biglist2, html2)
